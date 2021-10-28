6c425fbc20fa65c033ac4f35b4a251bdfd977cd3
Sancio <sancio@poczta.onet.pl>
2021-10-28 13:18:01 +0200

    Fix indent of return from function

--- plugin.py
+++ plugin.py
@@ -402,7 +402,7 @@ def CalculateBoilerSetPoint():
             TargetTemperature=Devices[MINBOILERTEMP].nValue
 
         #Return values
-            return True,TargetTemperature
+        return True,TargetTemperature
 
 def DomoticzAPI(APICall):
     resultJson = None
